ec8d2a75cff6d14d92a72e04b0d6aa093879a2bb
Zhao Shijin <Shijin.Zhao.hd@bmw-brilliance.cn>
2022-11-08 20:32:36 +0800

    Add comment for bikeshare.py station stats

--- bikeshare.py
+++ bikeshare.py
@@ -109,7 +109,7 @@ def time_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-
+# definate station_stats function
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 

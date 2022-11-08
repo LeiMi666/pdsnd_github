bb25b1cce6232e1d58fd2f63a2853e8e357b62ac
Zhao Shijin <Shijin.Zhao.hd@bmw-brilliance.cn>
2022-11-08 20:48:50 +0800

    Add comment to bikeshare.py trip duration stats

--- bikeshare.py
+++ bikeshare.py
@@ -132,7 +132,7 @@ def station_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-
+# definite trip_duration_stats function
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
